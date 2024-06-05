cb7d7f50c4c98e154c6b09e75bf27fec931d5074
Christian <bournecreative@gmail.com>
2024-06-05 07:59:04 -0500

    Add css file to specifiy map size and test out classes from main index

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,18 @@
 import { User } from './User'
 import { Company } from './Company'
+import { CustomMap } from './CustomMap';
+import "./css/styles.css"
 
 const testUser = new User();
 const testCompany = new Company();
+const testMap = new CustomMap();
+testMap.initMap()
+testMap.initMarker()
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div><h4>Name</h4>${testUser.name}</div>
     <div><h4>Name</h4>${testCompany.motto}</div>
+    <div id="map"></div>
   </div>
 `
-
-
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-    // <a href="https://vitejs.dev" target="_blank">
-    //   <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    // </a>
-    // <a href="https://www.typescriptlang.org/" target="_blank">
-    //   <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    // </a>
-    // <h1>Vite + TypeScript</h1>
-    // <div class="card">
-    //   <button id="counter" type="button"></button>
-    // </div>
-    // <p class="read-the-docs">
-    //   Click on the Vite and TypeScript logos to learn more
-    // </p>
